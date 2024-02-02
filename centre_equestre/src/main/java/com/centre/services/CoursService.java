0dadcdc14c7c4aa8cacf15c3071cce461a17e634
GIRARDIN Claire <116156608+GirardinClaire@users.noreply.github.com>
2024-02-02 18:51:17 +0100

    Add files via upload

---
+++ centre_equestre/src/main/java/com/centre/services/CoursService.java
@@ -0,0 +1,34 @@
+package com.centre.services;
+
+import com.centre.models.Cavalier;
+import com.centre.models.Cours;
+import com.centre.repositories.CavalierRepo;
+import com.centre.repositories.CoursRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class CoursService {
+
+    @Autowired
+    private CoursRepo repository;
+
+    public List<Cours> findAll() {
+        List<Cours> cours = (List<Cours>) repository.findAll();
+        return cours;
+    }
+
+    public void save(Cours cours) {  // pas testé avec persist !
+        repository.save(cours);
+    }
+
+    public Optional<Cours> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void delete(Cours cours) {
+        repository.delete(cours);
+    }
+}
