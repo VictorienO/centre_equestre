fdc8a93b235b3dca9ccbac19f706648386938490
VictorienO <127935325+VictorienO@users.noreply.github.com>
2024-02-02 19:51:15 +0100

    Delete centre_equestre directory

--- centre_equestre/src/main/java/com/centre/repositories/CoursRepo.java
+++
@@ -1,8 +0,0 @@
-package com.centre.repositories;
-
-import com.centre.models.Cours;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoursRepo extends CrudRepository<Cours, Long> {
-
-}
