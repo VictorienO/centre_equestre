f6541f60390395d3d1adccd39e8a57a64a4c31a0
GIRARDIN Claire <116156608+GirardinClaire@users.noreply.github.com>
2024-02-07 18:48:29 +0100

    Delete centre_equestre directory

--- centre_equestre/src/main/java/com/centre/repositories/DBUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.centre.repositories;
-
-import com.centre.models.DBUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface DBUserRepo extends JpaRepository<DBUser, Integer> {
-    public DBUser findByUsername(String username);
-}
